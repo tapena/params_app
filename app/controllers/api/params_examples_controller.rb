9b7a98745db149f0e76a2a3ebac9556f8eb48e4d
Tatiana Pena <Tatiana.c.pena1@gmail.com>
2018-09-10 20:50:31 -0500

    09.10.18/version2

--- app/controllers/api/params_examples_controller.rb
+++ app/controllers/api/params_examples_controller.rb
@@ -1,6 +1,6 @@
 class Api::ParamsExamplesController < ApplicationController
   def query_params_action
-    @input_message = params["my_message"]
+    @input_message = params["pineapple"]
     @street_sweeper = params["taco"]
     render "query_params_view.json.jbuilder"
   end
